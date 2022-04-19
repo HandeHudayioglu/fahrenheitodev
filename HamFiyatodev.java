d96f9aba55667e3004a83cff49f91d27fe588941
Handehdy <handehudayioglu@gmail.com>
2022-04-19 17:22:01 +0300

    first commit

---
+++ HamFiyatodev.java
@@ -0,0 +1,23 @@
+package com.bilgeadam.boost.java.course02.lesson007;
+
+import java.util.Scanner;
+
+public class HamFiyatodev {
+
+	public static void main(String[] args) {
+		
+		double x,y,z ; 
+		
+		
+		
+		Scanner satisFiyatı = new Scanner (System.in);
+	    System.out.print("Lütfen satış fiyatını giriniz:");
+	    x = satisFiyatı.nextInt();
+	    y=(x*100)/(100+18);
+	    z=(y*100)/(100+15);
+	    System.out.println("KDV'siz Fiyat: "+y);
+	    System.out.println("Ham Fiyat: " +z);
+
+	}
+
+}
