48b57035430142edd17efdfa3c407b9b58fff344
Handehdy <103752651+Handehdy@users.noreply.github.com>
2022-05-18 17:17:08 +0300

    Update ArithmeticExamples.java

--- ArithmeticExamples.java
+++ ArithmeticExamples.java
@@ -7,33 +7,20 @@ public class ArithmeticExamples {
 	public static void main(String[] args) {
 		
 		double x,y,z;
-//		Scanner scanner = new Scanner(System.in);
-//		
-//		System.out.println("Lütfen bir sayı giriniz:");
-//		
-//		x = scanner.nextInt();
-
-//	    System.out.println(x+1);
-//		Scanner scanner2 = new Scanner(System.in);
-//		
-//		System.out.println("Lütfen 2. sayıyı giriniz:");
-//		
-//		x=scanner.nextInt();
-//		System.out.println(x);
-	    
+		
 	    Scanner scanner = new Scanner (System.in);
-	    System.out.println("Lütfen ilk sayıyı giriniz:");
+	    System.out.println("Please enter the first number:");
 	    x = scanner.nextInt();
-	    System.out.println("Lütfen ikinci sayıyı giriniz:");
+	    System.out.println("Please enter the second numbaer:");
 	    y=scanner.nextInt();
-	    System.out.println("Lütfen üçüncü sayıyı giriniz");
+	    System.out.println("Please enter the third numbaer:");
 	    z=scanner.nextInt();
 	    
 	    System.out.println("Sonuç:"+x*(y+z));
 	    		
 	    	
 
-//        System.out.println("Bölüm: "+ x/y);
+//         System.out.println("Bölüm: "+ x/y);
 //	    System.out.println("Kalan: "+ x%y);
 
 		
